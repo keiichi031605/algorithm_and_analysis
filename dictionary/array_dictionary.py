31babe6e5a166a1c8752e9a58f54d4cedb0293b3
keiichi031605 <keichi031605@gmail.com>
2022-08-27 18:24:22 +1000

    Reimplemented build, search, add and delete of array dictionary

--- dictionary/array_dictionary.py
+++ dictionary/array_dictionary.py
@@ -15,19 +15,26 @@ class ArrayDictionary(BaseDictionary):
 
     def __init__(self):
         # pass
-        self.array_dictionary = None
+        self.array_dictionary = []
 
     def build_dictionary(self, words_frequencies: [WordFrequency]):
         """
         construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
-        # 1. set empty dictionary and build dictionary with for loop
-        wf_dictionary= {}
+        # # 1. set empty dictionary and build dictionary with for loop
+        # wf_dictionary= {}
+        # for wf in words_frequencies:
+        #     wf_dictionary[wf.word] = wf.frequency
+        # # 2. assign crated dictionary to self.array_dictionary
+        # self.array_dictionary = wf_dictionary
+
+        # 1. loop through a list of word_frequencies and append them to the array_dictionary
         for wf in words_frequencies:
-            wf_dictionary[wf.word] = wf.frequency
-        # 2. assign crated dictionary to self.array_dictionary
-        self.array_dictionary = wf_dictionary
+            self.array_dictionary.append((wf.word, wf.frequency))
+
+        # 2. sort by alphabetical order
+        self.array_dictionary.sort()
 
     def search(self, word: str) -> int:
         """
@@ -36,12 +43,19 @@ class ArrayDictionary(BaseDictionary):
         @return: frequency > 0 if found and 0 if NOT found
         """
         # 1. loop through array_dictionary to search word if it's contained
+        # count = 0
+        # for key, value in self.array_dictionary.items():
+        #     if word in key:
+        #         count += 1
+        # return count
+
+        # 1. loop through array_dictionary to search
         count = 0
-        for key, value in self.array_dictionary.items():
-            if word in key:
+        for wf in self.array_dictionary:
+            if word in wf[0]:
                 count += 1
 
-        return count    
+        return count
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
         """
@@ -50,13 +64,24 @@ class ArrayDictionary(BaseDictionary):
         :return: True whether succeeded, False when word is already in the dictionary
         """
         # 1. loop through array_dictionary to check word already exists
+        # word_validation = True
+        # for key, value in self.array_dictionary.items():
+        #     if word_frequency.word == key:
+        #         word_validation = False                
+        # # 2. if word doesn't exist, add the word and frequency to the array_dictionary
+        # if word_validation:
+        #     self.array_dictionary[word_frequency.word] = word_frequency.frequency
+        # return word_validation
+
+        # 1. loop through array_dictionary to check word already exists
         word_validation = True
-        for key, value in self.array_dictionary.items():
-            if word_frequency.word == key:
-                word_validation = False                
-        # 2. if word doesn't exist, add the word and frequency to the array_dictionary
+        for wf in self.array_dictionary:
+            if word_frequency.word == wf[0]:
+                word_validation = False
+        # 2. if word doesn't exist, add the word and frequency to the array_dictionary and sort it.
         if word_validation:
-            self.array_dictionary[word_frequency.word] = word_frequency.frequency
+            self.array_dictionary.append((word_frequency.word, word_frequency.frequency))
+            self.array_dictionary.sort()
 
         return word_validation
 
@@ -66,11 +91,38 @@ class ArrayDictionary(BaseDictionary):
         @param word: word to be deleted
         @return: whether succeeded, e.g. return False when point not found
         """
-        # find the position of 'word' in the list, if exists, will be at idx-1
-        # TO BE IMPLEMENTED
+        # 1. loop through the array dictionary to find the position of 'word' in the list with idx var
+        # word_validation = False
+        # count = -1
+        # idx = 0
+        # for key, value in self.array_dictionary.items():
+        #     count += 1
+        #     if word == key:
+        #         word_validation = True
+        #         idx = count
+        # print(word_validation)
+        # # 2. if word doesn't exist, add the word and frequency to the array_dictionary
+        # if word_validation:
+        #     print(self.array_dictionary[idx])
+        #     # self.array_dictionary[word_frequency.word] = word_frequency.frequency
 
-        return False
+        # return word_validation
 
+        # 1. loop through the array dictionary to find the position of 'word' in the list with idx var
+        word_validation = False
+        count = -1
+        idx = None
+        for wf in self.array_dictionary:
+            count += 1
+            if word == wf[0]:
+                word_validation = True
+                idx = count
+        # if word doesn't exist, remove the word and frequency from the array_dictionary by pop() with index
+        if idx:
+            self.array_dictionary.pop(idx)
+        
+        return word_validation
+        
 
     def autocomplete(self, prefix_word: str) -> [WordFrequency]:
         """
