201f005bfa8d077c4bdd09ef129c0822926c5e8d
keiichi031605 <keichi031605@gmail.com>
2022-08-27 16:30:56 +1000

    implemented add function of array dict

--- dictionary/array_dictionary.py
+++ dictionary/array_dictionary.py
@@ -22,9 +22,11 @@ class ArrayDictionary(BaseDictionary):
         construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
+        # 1. set empty dictionary and build dictionary with for loop
         wf_dictionary= {}
         for wf in words_frequencies:
             wf_dictionary[wf.word] = wf.frequency
+        # 2. assign crated dictionary to self.array_dictionary
         self.array_dictionary = wf_dictionary
 
     def search(self, word: str) -> int:
@@ -33,10 +35,12 @@ class ArrayDictionary(BaseDictionary):
         @param word: the word to be searched
         @return: frequency > 0 if found and 0 if NOT found
         """
+        # 1. loop through array_dictionary to search word if it's contained
         count = 0
         for key, value in self.array_dictionary.items():
             if word in key:
                 count += 1
+
         return count    
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
@@ -45,9 +49,16 @@ class ArrayDictionary(BaseDictionary):
         @param word_frequency: (word, frequency) to be added
         :return: True whether succeeded, False when word is already in the dictionary
         """
-        
-        
-        return False
+        # 1. loop through array_dictionary to check word already exists
+        word_validation = True
+        for key, value in self.array_dictionary.items():
+            if word_frequency.word == key:
+                word_validation = False                
+        # 2. if word doesn't exist, add the word and frequency to the array_dictionary
+        if word_validation:
+            self.array_dictionary[word_frequency.word] = word_frequency.frequency
+
+        return word_validation
 
     def delete_word(self, word: str) -> bool:
         """
